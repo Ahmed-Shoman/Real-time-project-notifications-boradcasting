7477ab156ae720227f55701e3c7a2103e07ea6ec
ahmed shoman <147621224+Ahmed-Shoman@users.noreply.github.com>
2025-12-06 01:01:49 +0200

    handle to send notification when a user registerd to admin

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use App\Models\User;
+use App\Notifications\NewUserRegisterdNotification;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -43,6 +47,8 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
+        Notification::send(Admin::all() , new NewUserRegisterdNotification($user));
+
 
         Auth::login($user);
 
