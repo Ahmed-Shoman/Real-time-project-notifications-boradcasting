8e0bbeeaf900804a0cc3579618288cafefc86972
ahmed shoman <147621224+Ahmed-Shoman@users.noreply.github.com>
2025-12-06 19:20:53 +0200

    fix the error when trying to rigister for new user

--- app/Notifications/NewUserRegisterdNotification.php
+++ app/Notifications/NewUserRegisterdNotification.php
@@ -28,19 +28,19 @@ class NewUserRegisterdNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database' , 'broadcast'];
     }
 
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
+    // public function toMail(object $notifiable): MailMessage
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to the notification.')
+    //                 ->action('Notification Action', url('/'))
+    //                 ->line('Thank you for using our application!');
+    // }
 
     /**
      * Get the array representation of the notification.
